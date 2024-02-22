7a8d1b7789e0360f0710617c3ebc1ff8f821200b
N8182 <145053798+N8182@users.noreply.github.com>
2024-02-22 00:02:45 -0500

    test2.java

--- test.java
+++ test.java
@@ -2,6 +2,6 @@
 public class test {
     public static void main(String[] args) {
         String code = "testing 1,2,3";
-        System.out.print(code);
+        System.out.println(code);
     }
 }
