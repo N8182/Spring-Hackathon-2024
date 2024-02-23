f71fa2215c19df66dbad5a1a342038988b6eba59
mike446 <mikiastadesse83@gmail.com>
2024-02-22 23:19:04 -0500

    Update test.java

--- test.java
+++ test.java
@@ -4,7 +4,7 @@ public class test {
     public static void main(String[] args) {
         String code = "testing 1,2,3";
         System.out.println(code);
-        Systen.out.println("Hi");
+        System.out.println("Hi");
 
     }
 
